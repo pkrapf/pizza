41f66a0002978db52f32deca1310576466a0def1
Paul Krapf <paulkrapf@gmail.com>
2019-05-08 09:29:21 -0500

    pizza

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PizzaMenuComponent } from './pizza-menu/pizza-menu.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { RegistrationComponent } from './registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: '', component: AboutUsComponent},
+{path: 'pizza-menu', component: PizzaMenuComponent},
+{path: 'check-out', component: CheckOutComponent},
+{path: 'registration', component: RegistrationComponent},
+{path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { PizzaMenuComponent } from './pizza-menu/pizza-menu.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RegistrationComponent } from './registration/registration.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PizzaMenuComponent,
+    AboutUsComponent,
+    CheckOutComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
